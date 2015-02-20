0396ce77375319834aca5c329e6f8a1ff4e83e9a
Tero Vuorela <tero.vuorela@iki.fi>
2015-02-20 15:04:32 +0200

    Removed duplication functionality

--- comms/src/main.cpp
+++ comms/src/main.cpp
@@ -1,14 +1,12 @@
 #include <QCoreApplication>
 #include <QObject>
 #include <QDebug>
-#include <QUrl>
 
-#include "gberrymaincontroller.h"
 #include "gberryconsoleserver.h"
 
-#include "restinvocationfactoryimpl.h"
-#include "serverconnectionimpl.h"
-#include "realsystemservices.h"
+#include <restinvocationfactoryimpl.h>
+#include <serverconnectionimpl.h>
+#include <realsystemservices.h>
 
 
 int main(int argc, char *argv[])
@@ -19,8 +17,6 @@ int main(int argc, char *argv[])
     systemServices.registerItself();
 
     GBerryConsoleServer server;
-    //QNetworkAccessManager* qnam = new QNetworkAccessManager();
-    //QNetworkAccessManager proxy(qnam);
 
     RESTInvocationFactoryImpl factory;
     factory.setProperty("url_prefix", "http://localhost:9000/gberryrest/v1");
@@ -39,10 +35,6 @@ int main(int argc, char *argv[])
                      &model,            &ServerConnectionModel::serverDisconnected);
     */
     serverConnection.open();
-
-    //GBerryMainController controller(&proxy);
-    //controller.start();
-    //controller.startRequest(QUrl("http://localhost:9000/gberryrest/v1/ping/"));
 
     return a.exec();
 }
