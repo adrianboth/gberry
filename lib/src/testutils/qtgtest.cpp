b26af41e5e7ccce02c84aa34acb0977887c16c4f
Tero Vuorela <tero.vuorela@iki.fi>
2015-02-28 10:44:27 +0200

    Moved gtest adapter for QString into *.cpp file

--- lib/src/testutils/qtgtest.cpp
+++ lib/src/testutils/qtgtest.cpp
@@ -1,1 +1,7 @@
+#include "qtgtest.h"
 
+// It's important that the << operator is defined in the SAME
+// namespace that defines Bar.  C++'s look-up rules rely on that.
+::std::ostream& operator<<(::std::ostream& os, const QString& bar) {
+  return os << qPrintable(bar);
+}
--- lib/src/testutils/qtgtest.h
+++ lib/src/testutils/qtgtest.h
@@ -1,5 +1,12 @@
 #ifndef QTGTEST_H
 #define QTGTEST_H
 
+#include <ostream>
+#include <QString>
+
+// It's important that the << operator is defined in the SAME
+// namespace that defines Bar.  C++'s look-up rules rely on that.
+::std::ostream& operator<<(::std::ostream& os, const QString& bar);
+
 #endif // QTGTEST_H
 
