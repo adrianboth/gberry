dc12b0c7a83590de1842e47fcddc12d67ee05e57
Tero Vuorela <tero.vuorela@iki.fi>
2015-03-08 13:52:10 +0200

    Logging

--- comms/src/main.cpp
+++ comms/src/main.cpp
@@ -12,16 +12,31 @@
 #include <server/serversetup.h>
 #include <server/playersessionmanager.h>
 
+#define LOG_AREA "comms"
+#include "log/log.h"
+#include "log/stdoutlogmsghandler.h"
+#include "log/logcontrol.h"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    StdoutLogMsgHandler handler(Log::TRACE);
+    LogControl logControl;
+    logControl.registerMsgHandler(&handler);
+    Log::singleton().use(&logControl);
+
+    INFO("Setting up ...");
 
     RealSystemServices systemServices;
     systemServices.registerItself();
 
     ServerSetup setup;
     setup.start();
+
+    // TODO: pinging gberry server is missing
+    INFO("Starting event queue");
 
     return a.exec();
 }
