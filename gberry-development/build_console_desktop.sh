9bb2fa23ebe357cb8fb5ce3a011b45a7110dca10
Tero Vuorela <tero.vuorela@iki.fi>
2015-04-02 17:40:00 +0300

    working scripts

--- gberry-development/build_console_desktop.sh
+++ gberry-development/build_console_desktop.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 THIS_DIR=`cd $(dirname $0) && pwd`
 WORKSPACE_ROOT=`cd $THIS_DIR/.. && pwd`
-
-QMAKE=/home/tevuore/Qt5.4/5.4/gcc_64/bin/qmake
-BUILD_NAME=Desktop_Qt_5_4_0_GCC_64bit-Debug
-CONFIG="CONFIG+=debug CONFIG+=declarative_debug CONFIG+=qml_debug"
-SPEC=linux-g++
 BUILDS_DIR=$THIS_DIR/../builds
 
 # -- Build
@@ -20,7 +15,7 @@ function build {
   REL_PRO_FILE=$2
   
   SRC_DIR=$WORKSPACE_ROOT/$COMP
-  BUILD_DIR=$BUILDS_DIR/build-${COMP}-Desktop_Qt_5_4_0_GCC_64bit-Debug
+  BUILD_DIR=$BUILDS_DIR/build-${COMP}-${BUILD_NAME}
   PRO_FILE=$WORKSPACE_ROOT/$REL_PRO_FILE
 
   if [ ! -e $BUILD_DIR ]; then
@@ -28,14 +23,51 @@ function build {
   fi
 
   cd $BUILD_DIR
-  $QMAKE $PRO_FILE -spec ${SPEC} ${CONFIG} BUILD_NAME=${BUILD_NAME} || die "QMake failed!"
+  $QMAKE $PRO_FILE ${SPEC} ${CONFIG} BUILD_NAME=${BUILD_NAME} || die "QMake failed!"
   /usr/bin/make || die "Make failed"
+  /usr/bin/make install || die "Make install failed"
 }
 
-build gberry-lib gberry-lib/gberry-lib.pro
-build console-lib gberry-console/console-lib/console-lib.pro
-build comms gberry-console/comms/comms.pro
-build mainui gberry-console/mainui/mainui.pro
+function build_desktop {
+  QMAKE=/home/tevuore/Qt5.4/5.4/gcc_64/bin/qmake
+  BUILD_NAME=Desktop_Qt_5_4_0_GCC_64bit-Debug
+  CONFIG="CONFIG+=debug CONFIG+=declarative_debug CONFIG+=qml_debug BUILD_NAME=$BUILD_NAME"
+  SPEC="-spec linux-g++"
 
-# TODO: installation? collecting files together
-# TODO: Qt
+  build gberry-lib gberry-lib/gberry-lib.pro
+  build console-lib gberry-console/console-lib/console-lib.pro
+  build comms gberry-console/comms/comms.pro
+  build mainui gberry-console/mainui/mainui.pro
+  
+  $WORKSPACE_ROOT/gberry-console/qt5rpi/deploy_qt.sh desktop || die "Failed to deploy Qt!"
+}
+
+function build_rpi {
+  QMAKE=/usr/local/qt5pi/bin/qmake
+  if [ ! -e /mnt/rasp-pi-rootfs ]; then
+    echo "ERROR: /mnt/rasp-pi-rootfs not mounted!"
+    echo "USE:   sudo mount -o loop,offset=62914560 2014-12-24-wheezy-raspbian.img /mnt/rasp-pi-rootfs"
+    exit 1
+  fi
+  BUILD_NAME=Raspberry-Debug
+  CONFIG="CONFIG+=debug CONFIG+=declarative_debug CONFIG+=qml_debug BUILD_NAME=$BUILD_NAME"
+  #SPEC=linux-g++
+  SPEC=""
+  
+  build gberry-lib gberry-lib/gberry-lib.pro
+  build console-lib gberry-console/console-lib/console-lib.pro
+  build comms gberry-console/comms/comms.pro
+  build mainui gberry-console/mainui/mainui.pro
+  $WORKSPACE_ROOT/gberry-console/qt5rpi/deploy_qt.sh rpi || die "Failed to deploy Qt!"
+}
+
+# TODO: These are debug builds: release also
+
+if [ "$1" == "desktop" ]; then
+  build_desktop
+elif [ "$1" == "rpi" ]; then
+  build_rpi
+else
+  build_desktop
+  build_rpi
+fi
--- gberry-development/build_mobapp_desktop.sh
+++ gberry-development/build_mobapp_desktop.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 THIS_DIR=`cd $(dirname $0) && pwd`
 WORKSPACE_ROOT=`cd $THIS_DIR/.. && pwd`
-
-QMAKE=/home/tevuore/Qt5.4/5.4/gcc_64/bin/qmake
-CONFIG="CONFIG+=debug CONFIG+=declarative_debug CONFIG+=qml_debug"
-SPEC=linux-g++
 BUILDS_DIR=$THIS_DIR/../builds
 
 # -- Build
@@ -19,7 +15,7 @@ function build {
   REL_PRO_FILE=$2
   
   SRC_DIR=$WORKSPACE_ROOT/$COMP
-  BUILD_DIR=$BUILDS_DIR/build-${COMP}-Desktop_Qt_5_4_0_GCC_64bit-Debug
+  BUILD_DIR=$BUILDS_DIR/build-${COMP}-${BUILD_NAME}
   PRO_FILE=$WORKSPACE_ROOT/$REL_PRO_FILE
 
   if [ ! -e $BUILD_DIR ]; then
@@ -31,7 +27,44 @@ function build {
   /usr/bin/make || die "Make failed"
 }
 
-build gberry-lib gberry-lib/gberry-lib.pro
-build mobapp gberry-mobile/mobapp/mobapp.pro
+function build_desktop {
+  # TODO: this should be configurable
+  QMAKE=/home/tevuore/Qt5.4/5.4/gcc_64/bin/qmake
+  SPEC=linux-g++
+  BUILD_NAME=Desktop_Qt_5_4_0_GCC_64bit-Debug
+  CONFIG="CONFIG+=debug CONFIG+=declarative_debug CONFIG+=qml_debug BUILD_NAME=$BUILD_NAME"
+  
+  build gberry-lib gberry-lib/gberry-lib.pro
+  build mobapp gberry-mobile/mobapp/mobapp.pro
+}
 
-# TODO: Qt
+function build_android {
+  QTDIR=/home/tevuore/Qt5.4/5.4/android_armv7
+  QMAKE=$QTDIR/bin/qmake
+  SPEC=android-g++
+  BUILD_NAME=Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_0-Debug
+  CONFIG="CONFIG+=debug CONFIG+=declarative_debug CONFIG+=qml_debug BUILD_NAME=$BUILD_NAME"
+
+  export ANDROID_NDK_ROOT=/home/tevuore/android/android-ndk-r9d
+  export ANDROID_SDK_ROOT=/home/tevuore/android/adt-bundle-linux-x86_64-20140702/sdk
+  
+  build gberry-lib gberry-lib/gberry-lib.pro
+  build mobapp gberry-mobile/mobapp/mobapp.pro
+  
+  # TODO: apk packaging
+  BUILD_DIR=${BUILDS_DIR}/build-mobapp-${BUILD_NAME}
+  ANDROID_BUILD=$BUILD_DIR/android-build
+  /usr/bin/make INSTALL_ROOT=$ANDROID_BUILD install
+  
+  # TODO: where this *.json comes?
+  $QTDIR/bin/androiddeployqt --input $BUILD_DIR/android-libmobapp.so-deployment-settings.json --output $ANDROID_BUILD --deployment bundled --android-platform android-20 --jdk /usr/lib/jvm/java-7-openjdk-amd64 --ant /usr/bin/ant
+}
+
+if [ "$1" == "desktop" ]; then
+  build_desktop
+elif [ "$1" == "android" ]; then
+  build_android
+else
+  build_desktop
+  build_android
+fi
