2818115dde067ae63bb9088a6875b2dc37c48b0c
Tero Vuorela <tero.vuorela@iki.fi>
2015-04-04 08:46:05 +0300

    Qt into LD_LIBRARY_PATH

--- comms/scripts/start.sh
+++ comms/scripts/start.sh
@@ -2,7 +2,14 @@
 THIS_DIR=`cd $(dirname $0) && pwd`
 CONSOLE_ROOT=`cd $THIS_DIR/.. && pwd`
 
-export LD_LIBRARY_PATH=$CONSOLE_ROOT/lib
+export LD_LIBRARY_PATH=$CONSOLE_ROOT/lib:$CONSOLE_ROOT/lib/Qt/5.4.0/lib
+
+QTDIR=`cd $THIS_DIR/../lib/Qt/5.4.0 && pwd`
+QTCONF=$THIS_DIR/qt.conf
+if [ ! -e $QTCONF ]; then
+    echo "[Paths]" > $QTCONF
+    echo "Prefix = $QTDIR" >> $QTCONF
+fi
 
 if [ -z "$1" ]; then
     $THIS_DIR/comms
