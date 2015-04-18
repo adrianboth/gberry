727fac648dfb4eda053d8ed7eaf250f654fc2d9a
Tero Vuorela <tero.vuorela@iki.fi>
2015-04-18 09:20:08 +0300

    Added custom appbox message

--- console-lib/src/ui/qml/GBerryConsole/js/Messages.js
+++ console-lib/src/ui/qml/GBerryConsole/js/Messages.js
@@ -5,4 +5,14 @@ function createCloseQuestionMsg() {
     return JSON.stringify(js)
 }
 
+function createShowAppBoxMsg() {
+    return JSON.stringify({action: "ShowAppBox"})
+}
+
+// CustomAppBox message carries whatever json message to appbox on client side
+function createCustomAppBoxMsg(appboxJS) {
+    var js = {action: "AppBoxMessage", data: appboxJS}
+    return JSON.stringify(js)
+}
+
 var CLOSE_QUESTION_MSG = createCloseQuestionMsg()
