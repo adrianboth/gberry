b9b817e871233561b7205ee715519445051f4af4
Tero Vuorela <tero.vuorela@iki.fi>
2015-04-26 09:44:22 +0300

    Appbox message added

--- mobapp/js/MobileClientMessages.js
+++ mobapp/js/MobileClientMessages.js
@@ -5,3 +5,8 @@ function createGeneralActionMessage(actionId) {
     return JSON.stringify(js)
 }
 
+// CustomAppBox message carries whatever json message to appbox on client side
+function createCustomAppBoxMsg(appboxJS) {
+    var js = {action: "AppBoxMessage", data: appboxJS}
+    return JSON.stringify(js)
+}
