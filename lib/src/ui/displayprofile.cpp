ff9099dd282722efc343643d0cd21f92f829a441
Tero Vuorela <tero.vuorela@iki.fi>
2015-05-23 07:44:12 +0300

    Impl of reading values from env

--- lib/src/ui/displayprofile.cpp
+++ lib/src/ui/displayprofile.cpp
@@ -1,8 +1,40 @@
 #include "displayprofile.h"
 
-DisplayProfile::DisplayProfile()
-{
+#include <QProcessEnvironment>
 
+// TODO: later support for reading from a file
+namespace {
+    // TODO: these should on header side because of documentation!!
+
+    static const char* PREF_WINDOW_WIDTH_VARNAME = "PREF_WINDOW_WIDTH";
+    static const char* PREF_WINDOW_HEIGHT_VARNAME = "PREF_WINDOW_HEIGHT";
+    static const char* WINDOW_SCALE_FACTOR_VARNAME = "WINDOW_SCALE_FACTOR";
+
+}
+DisplayProfile::DisplayProfile(int defaultWindowWidth,
+                               int defaultWindowHeight,
+                               Mode defaultMode,
+                               double defaultSizeScaleFactor,
+                               QObject *parent) :
+    QObject(parent),
+    _mode(defaultMode),
+    _preferredWindowWidth(defaultWindowWidth),
+    _preferredWindowHeight(defaultWindowHeight),
+    _sizeScaleFactor(defaultSizeScaleFactor)
+{
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+
+    if (env.contains(PREF_WINDOW_WIDTH_VARNAME)) {
+        _preferredWindowWidth = env.value(PREF_WINDOW_WIDTH_VARNAME).toInt();
+    }
+
+    if (env.contains(PREF_WINDOW_HEIGHT_VARNAME)) {
+        _preferredWindowHeight = env.value(PREF_WINDOW_HEIGHT_VARNAME).toInt();
+    }
+
+    if (env.contains(WINDOW_SCALE_FACTOR_VARNAME)) {
+        _sizeScaleFactor = env.value(WINDOW_SCALE_FACTOR_VARNAME).toDouble();
+    }
 }
 
 DisplayProfile::~DisplayProfile()
@@ -10,3 +42,4 @@ DisplayProfile::~DisplayProfile()
 
 }
 
+// TODO: if real device then match real screen properties
--- lib/src/ui/displayprofile.h
+++ lib/src/ui/displayprofile.h
@@ -3,32 +3,51 @@
 
 #include <QObject>
 
+/**
+ * This class support development time scaling of display to match
+ * target hardware display specifications.
+ *
+ */
 class DisplayProfile : public QObject
 {
     Q_OBJECT
+
+    enum Mode { PixelMatch, DimensionMatch };
+
+    Q_PROPERTY(Mode mode READ mode NOTIFY modeChanged)
+    Q_PROPERTY(int windowWidth READ windowWidth NOTIFY windowWidthChanged)
+    Q_PROPERTY(int windowHeight READ windowHeight NOTIFY windowHeightChanged)
+    Q_PROPERTY(double sizeScaleFactor READ sizeScaleFactor NOTIFY sizeScaleFactorChanged)
+
+
 public:
-    DisplayProfile();
+    DisplayProfile(int defaultWindowWidth,
+                   int defaultWindowHeight,
+                   Mode defaultMode = DisplayProfile::PixelMatch,
+                   double defaultSizeScaleFactor = 1.0,
+                   QObject* parent = 0);
+
     ~DisplayProfile();
 
-/*
-    QQmlPropertyMap screenProps;
-    screenProps.insert("name", QVariant(QString("John Smith")));
+    Mode mode() const { return _mode; }
+    // TODO: implement dimensions mode
+    int windowWidth() const { return _preferredWindowWidth * _sizeScaleFactor; }
+    int windowHeight() const { return _preferredWindowHeight * _sizeScaleFactor; }
+    double sizeScaleFactor() const { return _sizeScaleFactor; }
 
-    // TODO: read for environment
-    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
 
-    if (env.contains("PREF_WINDOW_WIDTH")) {
-        screenProps.insert("preferredWindowWidth", QVariant(env.value("PREF_WINDOW_WIDTH").toInt()));
-    } else {
-        screenProps.insert("preferredWindowWidth", QVariant(320));
-    }
+signals:
+    void modeChanged();
+    void windowWidthChanged();
+    void windowHeightChanged();
+    void sizeScaleFactorChanged();
 
-    if (env.contains("PREF_WINDOW_HEIGHT")) {
-        screenProps.insert("preferredWindowHeight", QVariant(env.value("PREF_WINDOW_HEIGHT").toInt()));
-    } else {
-        screenProps.insert("preferredWindowHeight", QVariant(400));
-    }
-*/
+private:
+    Mode _mode;
+    int _preferredWindowWidth; // this just target, scale factor affect valid value
+    int _preferredWindowHeight; // this just target, scale factor affect valid value
+    double _sizeScaleFactor;
+
 };
 
 #endif // DISPLAYPROFILE_H
