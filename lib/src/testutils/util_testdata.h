55983d995fbf2d9b040129b36e0cd0d4f7c2c652
Tero Vuorela <tero.vuorela@iki.fi>
2015-06-04 10:44:40 +0300

    final impl

--- lib/src/testutils/util_testdata.h
+++ lib/src/testutils/util_testdata.h
@@ -1,5 +1,19 @@
 #ifndef UTIL_TESTDATA_H
 #define UTIL_TESTDATA_H
 
+#include <QDir>
+
+// trick to get define as a string
+#define xstr(s) str(s)
+#define str(s) #s
+
+namespace TestUtils
+{
+QString testdataDirPath(QString caseName)
+{
+    QDir testdataRoot = QDir(xstr(TESTDATA_ROOT));
+    return testdataRoot.filePath(caseName);
+}
+} // eon
 #endif // UTIL_TESTDATA_H
 
