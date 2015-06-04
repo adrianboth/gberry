368154ad0d2fa0821cef44aee1e0c2092375c9fd
Tero Vuorela <tero.vuorela@iki.fi>
2015-06-04 10:43:04 +0300

    final impl

--- lib/src/testutils/util_enablelog.cpp
+++ lib/src/testutils/util_enablelog.cpp
@@ -1,1 +1,23 @@
+#include "util_enablelog.h"
 
+namespace TestUtils
+{
+StdoutLoggingEnabler& enabledStdoutLogging()
+{
+    static StdoutLoggingEnabler enabler; // not thread safe
+    return enabler;
+}
+
+StdoutLoggingEnabler::StdoutLoggingEnabler()
+{
+    StdoutLogMsgHandler* handler = new StdoutLogMsgHandler(Log::TRACE);
+    LogControl* logControl = new LogControl();
+    logControl->registerMsgHandler(handler);
+    Log::singleton().use(logControl);
+}
+
+StdoutLoggingEnabler::~StdoutLoggingEnabler()
+{
+
+}
+} // eon
--- lib/src/testutils/util_enablelog.h
+++ lib/src/testutils/util_enablelog.h
@@ -1,1 +1,27 @@
+#ifndef UTIL_ENABLELOG_H
+#define UTIL_ENABLELOG_H
+
+#include "log/log.h"
+#include "log/stdoutlogmsghandler.h"
+#include "log/logcontrol.h"
+
+namespace TestUtils
+{
+// utility class to make call via object creation
+class StdoutLoggingEnabler
+{
+public:
+    StdoutLoggingEnabler();
+    ~StdoutLoggingEnabler();
+};
+
+// enables logging to stdout
+StdoutLoggingEnabler& enabledStdoutLogging();
+
+} // eon
+
+
+// TODO: teardown of logging (in desctructor)
+
+#endif // UTIL_ENABLELOG_H
 
