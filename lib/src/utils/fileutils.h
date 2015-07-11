3d6133c8ab22a5571c08a7c85071ca3c8ded8bc8
Tero Vuorela <tero.vuorela@iki.fi>
2015-07-11 07:35:57 +0300

    Added recursive copying and pushed impl to *.cpp file

--- lib/src/utils/fileutils.h
+++ lib/src/utils/fileutils.h
@@ -6,9 +6,12 @@
 
 namespace GBerryLib
 {
-QString joinpath (QString appDir, QString path) {
-    return QDir(appDir).absoluteFilePath(path);
-}
+QString joinpath (QString appDir, QString path);
+
+//https://qt.gitorious.org/qt-creator/qt-creator/source/1a37da73abb60ad06b7e33983ca51b266be5910e:src/app/main.cpp#L13-189
+// taken from utils/fileutils.cpp. We can not use utils here since that depends app_version.h.
+bool copyRecursively(const QString &srcFilePath,
+                            const QString &tgtFilePath);
 
 } // eon
 
