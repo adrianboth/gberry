84dd830b942b75a974beb52b7e3b31b2e94baf0d
Tero Vuorela <tero.vuorela@iki.fi>
2015-07-18 08:53:12 +0300

    code beautifying

--- waitapp/main.cpp
+++ waitapp/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     ApplicationMain main(&app);
 
     ConsoleApplication consoleApp;
+
     if (main.hasApplicationCode())
         consoleApp.setApplicationCode(main.applicationCode());
 
