46c7b624848c4bda01cb3056721ee68168a23592
Tero Vuorela <tero.vuorela@iki.fi>
2015-07-24 12:17:55 +0300

    Added copyright and license ref

--- console-lib/src/ui/qml/GBerryConsole/js/AppBoxMaster.js
+++ console-lib/src/ui/qml/GBerryConsole/js/AppBoxMaster.js
@@ -1,4 +1,22 @@
-.import GBerry.Log 1.0 as Log
+/* This file is part of GBerry.
+ *
+ * Copyright 2015 Tero Vuorela
+ *
+ * GBerry is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * GBerry is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GBerry. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ .import GBerry.Log 1.0 as Log
 //Log.initLog("AppBoxMaster", gsettings.logLevel)
 
 var _appboxResourceString
