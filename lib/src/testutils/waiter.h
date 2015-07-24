310b9f27ceec9714c93cce765050f6b286d26c35
Tero Vuorela <tero.vuorela@iki.fi>
2015-07-24 10:28:59 +0300

    Added copyright and licence ref

--- lib/src/testutils/waiter.h
+++ lib/src/testutils/waiter.h
@@ -1,4 +1,22 @@
-/**
+/* This file is part of GBerry.
+ *
+ * Copyright 2015 Tero Vuorela
+ *
+ * GBerry is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * GBerry is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GBerry. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ /**
  * Waiter class provides utility for processing Qt event loop messages while
  * waiting certain condition to fullfill. Typical use case in unit tests is
  * wait certain signal to be triggered.
