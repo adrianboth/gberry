cb24189784d6c952e45b7bffb623b2dc089ff9e4
Tero Vuorela <tero.vuorela@iki.fi>
2015-08-13 14:44:53 +0300

    Added info printing

--- development/copy_mobapp_to_deploy.sh
+++ development/copy_mobapp_to_deploy.sh
@@ -8,8 +8,8 @@ COMP=mobapp
 BUILD_DIR=$BUILDS_DIR/build-${COMP}-${ANDROID_BUILD_NAME}
 DST_FILENAME=gberry.apk
 
+echo "Copying to mobapp *.apk to deploy directories ..."
 cp $BUILD_DIR/android-build/bin/QtApp-debug.apk $BUILDS_DIR/deploy-console-Raspberry-Release/www/${DST_FILENAME}
 cp $BUILD_DIR/android-build/bin/QtApp-debug.apk $BUILDS_DIR/deploy-console-Desktop_Qt_5_4_0_GCC_64bit-Debug/www/${DST_FILENAME}
-
 
 echo "[DONE]"
