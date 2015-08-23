80891db1ef3ada8cb8a27742b53b1b41f1d0ecb2
Tero Vuorela <tero.vuorela@iki.fi>
2015-08-23 18:52:19 +0300

    Using filab server

--- development/python/settings.py
+++ development/python/settings.py
@@ -2,6 +2,7 @@
 USER_EMAIL = 'demo@idm.org'
 USER_PASSWORD = 'demonstration'
 
-SERVER_HOST = 'localhost'
+#SERVER_HOST = 'localhost'
+SERVER_HOST = '130.206.118.74'
 URL_PREFIX = "http://" + SERVER_HOST + "/gberryrest/v1"
 
